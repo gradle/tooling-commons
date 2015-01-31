d62b16727379f5f03871995c0b6e14cd9c5f8352
etiennestuder <etienne.studer@gradleware.com>
2015-01-31 09:38:21 +0100

    Add initial empty version

---
+++ toolingutils/src/main/java/com/gradleware/tooling/utils/distribution/GradlePublishedVersions.java
@@ -0,0 +1,8 @@
+package com.gradleware.tooling.utils.distribution;
+
+/**
+ * Provides information about the Gradle versions available form gradle.org.
+ */
+public final class GradlePublishedVersions {
+
+}
