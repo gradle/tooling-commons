871e1f185bb7250124acba7d228abb71c91210a8
etiennestuder <etienne@studer.nu>
2015-06-08 17:56:47 +0200

    Fix checkstyle

--- toolingmodel/src/main/java/com/gradleware/tooling/toolingmodel/repository/ModelRepositoryProviderFactory.java
+++ toolingmodel/src/main/java/com/gradleware/tooling/toolingmodel/repository/ModelRepositoryProviderFactory.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2015 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 \(the "License"\);
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -23,6 +23,8 @@ import org.gradle.internal.Factory;
 
 /**
  * Factory class to create {@link ModelRepositoryProvider} instances.
+ *
+ * @author Etienne Studer
  */
 public final class ModelRepositoryProviderFactory {
 
