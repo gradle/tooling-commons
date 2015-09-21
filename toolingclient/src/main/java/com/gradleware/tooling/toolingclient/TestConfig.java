94a08feaa3ce49379e770ab781764f5883c4c4bb
Donat Csikos <csdonat@gmail.com>
2015-09-21 11:25:07 +0200

    Add API to execute test methods

--- toolingclient/src/main/java/com/gradleware/tooling/toolingclient/TestConfig.java
+++ toolingclient/src/main/java/com/gradleware/tooling/toolingclient/TestConfig.java
@@ -18,11 +18,14 @@ package com.gradleware.tooling.toolingclient;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+
 import org.gradle.tooling.TestLauncher;
 import org.gradle.tooling.events.test.TestOperationDescriptor;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Encapsulates the {@link TestOperationDescriptor} instances to execute through a Gradle build.
@@ -33,16 +36,18 @@ public final class TestConfig {
 
     private final ImmutableList<String> jvmTestClasses;
     private final ImmutableList<? extends TestOperationDescriptor> tests;
+    private final ImmutableMap<String, List<String>> jvmTestMethods;
 
-    private TestConfig(List<String> jvmTestClasses, List<? extends TestOperationDescriptor> tests) {
+    private TestConfig(List<String> jvmTestClasses, List<? extends TestOperationDescriptor> tests, Map<String, List<String>> jvmTestMethods) {
         this.jvmTestClasses = ImmutableList.copyOf(jvmTestClasses);
         this.tests = ImmutableList.copyOf(tests);
+        this.jvmTestMethods = ImmutableMap.copyOf(jvmTestMethods);
 
-        checkNotAllListsEmpty(jvmTestClasses, tests);
+        checkNotAllListsEmpty(jvmTestClasses, tests, jvmTestMethods);
     }
 
-    private void checkNotAllListsEmpty(List<String> jvmTestClasses, List<? extends TestOperationDescriptor> tests) {
-        Preconditions.checkArgument(!jvmTestClasses.isEmpty() || !tests.isEmpty(), "Either JVM test classes or test operations, or both, must be specified.");
+    private void checkNotAllListsEmpty(List<String> jvmTestClasses, List<? extends TestOperationDescriptor> tests, Map<String, List<String>> jvmTestMethods) {
+        Preconditions.checkArgument(!jvmTestClasses.isEmpty() || !tests.isEmpty() || !jvmTestMethods.isEmpty(), "Either JVM test classes or test operations, or both, must be specified.");
     }
 
     /**
@@ -54,6 +59,9 @@ public final class TestConfig {
         Preconditions.checkNotNull(testLauncher);
         testLauncher.withJvmTestClasses(this.jvmTestClasses);
         testLauncher.withTests(this.tests);
+        for(String jvmTestClass : this.jvmTestMethods.keySet()) {
+            testLauncher.withJvmTestMethods(jvmTestClass, this.jvmTestMethods.get(jvmTestClass));
+        }
     }
 
     /**
@@ -97,16 +105,57 @@ public final class TestConfig {
     }
 
     /**
+     * Specifies the tests to be executed.
+     *
+     * @param jvmTestClass the name of the class containing the test methods
+     * @param jvmTestMethods the test method names to execute
+     * @return a new instance
+     */
+    public static TestConfig forJvmTestMethods(String jvmTestClass, String... jvmTestMethods) {
+        return new Builder().jvmTestMethods(jvmTestClass, Arrays.asList(jvmTestMethods)).build();
+    }
+
+    /**
+     * Specifies the tests to be executed.
+     *
+     * @param jvmTestClass the name of the class containing the test methods
+     * @param jvmTestMethods the test method names to execute
+     * @return a new instance
+     */
+    public static TestConfig forJvmTestMethods(String jvmTestClass, Iterable<String> jvmTestMethods) {
+        return new Builder().jvmTestMethods(jvmTestClass, jvmTestMethods).build();
+    }
+
+    /**
+     * Specifies the tests to be executed.
+     * <p/>
+     * The tests are specified in a map where a key is a name of a class and a value is a list of
+     * test method names under that class.
+     *
+     * @param jvmTestMethods the map of class name - method names to execute
+     * @return a new instance
+     */
+    public static TestConfig forJvmTestMethods(Map<String, Iterable<String>> jvmTestMethods) {
+        Builder result = new Builder();
+        for (String jvmTestClass : jvmTestMethods.keySet()) {
+            result.jvmTestMethods(jvmTestClass, jvmTestMethods.get(jvmTestClass));
+        }
+        return result.build();
+    }
+
+    /**
      * Builder to build {@code TestConfig} instances that accumulate multiple types of tests to execute.
      */
     public static final class Builder {
 
         private ImmutableList.Builder<String> jvmTestClasses;
         private ImmutableList.Builder<TestOperationDescriptor> tests;
+        private ImmutableMap.Builder<String, List<String>> jvmTestMethods;
 
         public Builder() {
             this.jvmTestClasses = ImmutableList.builder();
             this.tests = ImmutableList.builder();
+            this.jvmTestMethods = ImmutableMap.builder();
         }
 
         /**
@@ -143,6 +192,18 @@ public final class TestConfig {
         }
 
         /**
+         * Adds the test methods to be executed.
+         *
+         * @param jvmTestClass the container class where the methods are defined
+         * @param jvmTestMethods the methods to be executed
+         * @return this builder
+         */
+        public Builder jvmTestMethods(String jvmTestClass, Iterable<String> jvmTestMethods) {
+            this.jvmTestMethods.put(jvmTestClass, ImmutableList.copyOf(jvmTestMethods));
+            return this;
+        }
+
+        /**
          * Adds the tests to be executed.
          *
          * @param tests the tests to be executed
@@ -159,7 +220,7 @@ public final class TestConfig {
          * @return a new {@code TestConfig} instance
          */
         public TestConfig build() {
-            return new TestConfig(this.jvmTestClasses.build(), this.tests.build());
+            return new TestConfig(this.jvmTestClasses.build(), this.tests.build(), this.jvmTestMethods.build());
         }
 
     }
