b27627343d1048135e08eeb63bf9193b2193cebd
Stefan Oehme <st.oehme@gmail.com>
2016-02-03 14:19:44 -0500

    Remove jarjar imports

--- toolingclient/src/main/java/com/gradleware/tooling/toolingclient/internal/DefaultToolingClient.java
+++ toolingclient/src/main/java/com/gradleware/tooling/toolingclient/internal/DefaultToolingClient.java
@@ -21,8 +21,6 @@ import java.util.Map;
 import java.util.Set;
 
 import org.gradle.internal.Factory;
-import org.gradle.jarjar.com.google.common.collect.Lists;
-import org.gradle.jarjar.com.google.common.collect.Sets;
 import org.gradle.tooling.BuildAction;
 import org.gradle.tooling.BuildActionExecuter;
 import org.gradle.tooling.BuildLauncher;
@@ -43,7 +41,9 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Objects;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 import com.gradleware.tooling.toolingclient.BuildActionRequest;
 import com.gradleware.tooling.toolingclient.BuildLaunchRequest;
