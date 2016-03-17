336135ed9eecffb22b42a3d423c44e1b30506651
etiennestuder <etienne@gradle.com>
2016-03-17 06:23:20 +0100

    Increase performance by initializing with 0-length array

--- toolingclient/src/main/java/com/gradleware/tooling/toolingclient/internal/BaseCompositeRequest.java
+++ toolingclient/src/main/java/com/gradleware/tooling/toolingclient/internal/BaseCompositeRequest.java
@@ -54,7 +54,7 @@ abstract class BaseCompositeRequest<T, SELF extends BaseCompositeRequest<T, SELF
 
     @Override
     public GradleBuildIdentifier[] getParticipants() {
-        return this.participants.toArray(new GradleBuildIdentifier[this.participants.size()]);
+        return this.participants.toArray(new GradleBuildIdentifier[0]);
     }
 
     @Override
