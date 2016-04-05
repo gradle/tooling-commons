866e19894271a47d52ae2ccf6b5c02a95cf78d78
Stefan Oehme <st.oehme@gmail.com>
2016-04-05 14:42:50 -0400

    Cleanup DefaultOmniBuildInvocationsContainerBuilder

--- toolingmodel/src/main/java/com/gradleware/tooling/toolingmodel/repository/internal/DefaultOmniBuildInvocationsContainerBuilder.java
+++ toolingmodel/src/main/java/com/gradleware/tooling/toolingmodel/repository/internal/DefaultOmniBuildInvocationsContainerBuilder.java
@@ -16,30 +16,32 @@
 
 package com.gradleware.tooling.toolingmodel.repository.internal;
 
-import com.google.common.base.Preconditions;
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMultimap;
-import com.google.common.collect.ImmutableSortedMap;
-import com.google.common.collect.ImmutableSortedSet;
-import com.google.common.collect.Multimap;
-import com.google.common.collect.Ordering;
-import com.google.common.collect.Sets;
-import com.google.common.collect.Table;
-import com.google.common.collect.TreeBasedTable;
-import com.gradleware.tooling.toolingmodel.OmniBuildInvocations;
-import com.gradleware.tooling.toolingmodel.OmniProjectTask;
-import com.gradleware.tooling.toolingmodel.OmniTaskSelector;
-import com.gradleware.tooling.toolingmodel.Path;
-import org.gradle.tooling.model.DomainObjectSet;
-import org.gradle.tooling.model.GradleProject;
-import org.gradle.tooling.model.GradleTask;
-
-import java.util.Collection;
 import java.util.Comparator;
 import java.util.Set;
 import java.util.SortedMap;
 import java.util.SortedSet;
+
+import org.gradle.tooling.model.GradleProject;
+import org.gradle.tooling.model.GradleTask;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.ImmutableMultimap.Builder;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSortedMap;
+import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Ordering;
+import com.google.common.collect.Sets;
+import com.google.common.collect.TreeBasedTable;
+
+import com.gradleware.tooling.toolingmodel.OmniBuildInvocations;
+import com.gradleware.tooling.toolingmodel.OmniProjectTask;
+import com.gradleware.tooling.toolingmodel.OmniTaskSelector;
+import com.gradleware.tooling.toolingmodel.Path;
 
 /**
  * Builds a {@code DefaultOmniBuildInvocationsContainer} from a given Gradle project.
@@ -47,9 +49,6 @@ import java.util.SortedSet;
  * @author Etienne Studer
  */
 public final class DefaultOmniBuildInvocationsContainerBuilder {
-
-    @SuppressWarnings("RedundantStringConstructorCall")
-    private static final String NULL_STRING = new String(); // ensure unique instance to use it as a null-string placeholder
 
     /**
      * Converts a {@link GradleProject} to a {@link DefaultOmniBuildInvocationsContainer}.
@@ -59,8 +58,8 @@ public final class DefaultOmniBuildInvocationsContainerBuilder {
      * @return the build invocations container
      */
     public static DefaultOmniBuildInvocationsContainer build(GradleProject project, boolean enforceAllTasksPublic) {
-        ImmutableMultimap<Path, OmniProjectTask> tasks = buildProjectTasksRecursively(project, ArrayListMultimap.<Path, OmniProjectTask>create(), enforceAllTasksPublic);
-        ImmutableMultimap<Path, OmniTaskSelector> taskSelectors = buildTaskSelectorsRecursively(project, ArrayListMultimap.<Path, OmniTaskSelector>create(), enforceAllTasksPublic);
+        ImmutableMultimap<Path, OmniProjectTask> tasks = getAllProjectTasksByProjectPath(project, enforceAllTasksPublic);
+        ImmutableMultimap<Path, OmniTaskSelector> taskSelectors = getAllTaskSelectorsByProjectPath(project, enforceAllTasksPublic);
         ImmutableSortedMap<Path, OmniBuildInvocations> buildInvocationsPerProject = buildBuildInvocationsMapping(project, tasks, taskSelectors);
         return DefaultOmniBuildInvocationsContainer.from(buildInvocationsPerProject);
     }
@@ -81,93 +80,82 @@ public final class DefaultOmniBuildInvocationsContainerBuilder {
         // create additional mappings for all those projects which do not contain any task selectors
         // this is the case if a project does not contain any tasks nor does any of its child projects
         // these additional mappings ensure the caller never gets back null for any project in the hierarchy
-        Set<Path> projectPaths = Sets.newLinkedHashSet();
-        collectProjectPathsRecursively(project, projectPaths);
-        projectPaths.removeAll(taskSelectors.keySet());
-        for (Path projectPath : projectPaths) {
+        Set<Path> projectsWithoutTaskSelectors = Sets.difference(getAllProjectPaths(project), taskSelectors.keySet());
+        for (Path projectPath : projectsWithoutTaskSelectors) {
             mapping.put(projectPath, DefaultOmniBuildInvocations.from(ImmutableList.<OmniProjectTask>of(), ImmutableList.<OmniTaskSelector>of()));
         }
 
         return mapping.build();
     }
 
-    private static void collectProjectPathsRecursively(GradleProject project, Set<Path> projectPaths) {
-        // add project's path
+    private static ImmutableSet<Path> getAllProjectPaths(GradleProject project) {
+        ImmutableSet.Builder<Path> projectPaths = ImmutableSet.builder();
         projectPaths.add(Path.from(project.getPath()));
 
-        // recurse into child projects and add their path
         for (GradleProject childProject : project.getChildren()) {
-            collectProjectPathsRecursively(childProject, projectPaths);
+            projectPaths.addAll(getAllProjectPaths(childProject));
         }
+        return projectPaths.build();
     }
 
-    private static ImmutableMultimap<Path, OmniProjectTask> buildProjectTasksRecursively(GradleProject project, Multimap<Path, OmniProjectTask> tasksPerProject, boolean enforceAllTasksPublic) {
-        // add tasks of the current project
+    private static ImmutableMultimap<Path, OmniProjectTask> getAllProjectTasksByProjectPath(GradleProject project, boolean enforceAllTasksPublic) {
+        Builder<Path, OmniProjectTask> tasks = ImmutableMultimap.builder();
+
         for (GradleTask task : project.getTasks()) {
-            tasksPerProject.put(Path.from(project.getPath()), DefaultOmniProjectTask.from(task, enforceAllTasksPublic));
+            tasks.put(Path.from(project.getPath()), DefaultOmniProjectTask.from(task, enforceAllTasksPublic));
         }
 
-        // recurse into child projects and add their tasks
-        for (GradleProject childProject : project.getChildren()) {
-            buildProjectTasksRecursively(childProject, tasksPerProject, enforceAllTasksPublic);
+        for (GradleProject child : project.getChildren()) {
+            tasks.putAll(getAllProjectTasksByProjectPath(child, enforceAllTasksPublic));
         }
 
-        // return the tasks grouped by project path
-        return ImmutableMultimap.copyOf(tasksPerProject);
+        return tasks.build();
     }
 
-    @SuppressWarnings("StringEquality")
-    private static ImmutableMultimap<Path, OmniTaskSelector> buildTaskSelectorsRecursively(GradleProject project, Multimap<Path, OmniTaskSelector> taskSelectorsPerProject, boolean enforceAllTasksPublic) {
-        // add task selectors of the current project
-        TreeBasedTable<String, Path, String> aggregatedTasksWithDescription = TreeBasedTable.create(Ordering.usingToString(), Path.Comparator.INSTANCE);
-        Set<String> publicTasks = Sets.newLinkedHashSet();
-        collectAllTasksRecursively(project, aggregatedTasksWithDescription, publicTasks, enforceAllTasksPublic);
-        for (String selectorName : aggregatedTasksWithDescription.rowKeySet()) {
-            SortedMap<Path, String> pathsAndDescriptions = aggregatedTasksWithDescription.row(selectorName);
-            String description = pathsAndDescriptions.get(pathsAndDescriptions.firstKey()); // description from project task with smallest path
-            SortedSet<Path> fqnTaskNames = ImmutableSortedSet.orderedBy(Path.Comparator.INSTANCE).addAll(pathsAndDescriptions.keySet()).build();
+    private static ImmutableMultimap<Path, OmniTaskSelector> getAllTaskSelectorsByProjectPath(GradleProject project, boolean enforceAllTasksPublic) {
+        Builder<Path, OmniTaskSelector> taskSelectors = ImmutableMultimap.builder();
+        TreeBasedTable<String, Path, OmniProjectTask> tasksByNameAndPath = getAllProjectTasksByNameAndPath(project, enforceAllTasksPublic);
+
+        for (String selectorName : tasksByNameAndPath.rowKeySet()) {
+            SortedMap<Path, OmniProjectTask> tasksByPath = tasksByNameAndPath.row(selectorName);
+            OmniProjectTask taskWithShortesPath = tasksByPath.get(tasksByPath.firstKey());
+            boolean isPublic = enforceAllTasksPublic || Iterables.any(tasksByPath.values(), new Predicate<OmniProjectTask>() {
+
+                @Override
+                public boolean apply(OmniProjectTask input) {
+                    return input.isPublic();
+                }
+            });
+            SortedSet<Path> selectedPaths = ImmutableSortedSet.copyOf(tasksByPath.comparator(), tasksByPath.keySet());
 
             OmniTaskSelector taskSelector = DefaultOmniTaskSelector.from(
                     selectorName,
-                    description != NULL_STRING ? description : null,
+                    taskWithShortesPath.getDescription(),
                     Path.from(project.getPath()),
-                    publicTasks.contains(selectorName),
-                    fqnTaskNames);
+                    isPublic,
+                    selectedPaths);
 
-            taskSelectorsPerProject.put(Path.from(project.getPath()), taskSelector);
-        }
-
-        // recurse into child projects and add their task selectors
-        DomainObjectSet<? extends GradleProject> childProjects = project.getChildren();
-        for (GradleProject childProject : childProjects) {
-            buildTaskSelectorsRecursively(childProject, taskSelectorsPerProject, enforceAllTasksPublic);
-        }
-
-        // return the task selectors grouped by project path
-        return ImmutableMultimap.copyOf(taskSelectorsPerProject);
-    }
-
-    private static void collectAllTasksRecursively(GradleProject project, Table<String, Path, String> tasksWithDescription, Collection<String> publicTasks, boolean enforceAllTasksPublic) {
-        for (GradleTask task : project.getTasks()) {
-            // convert to OmniProjectTask to have the version-specific logic and default-values applied
-            OmniProjectTask projectTask = DefaultOmniProjectTask.from(task, enforceAllTasksPublic);
-
-            // 1) store the path since the task selectors keep all the paths of the tasks they select
-            // 2) store the description first by task name and then by path
-            //      this allows to later fish out the description of the task whose name matches the selector name and
-            //      whose path is the smallest for the given task name (the first entry of the table column)
-            //      store null description as empty string to avoid that Guava chokes
-            tasksWithDescription.put(projectTask.getName(), projectTask.getPath(), projectTask.getDescription() != null ? projectTask.getDescription() : NULL_STRING);
-
-            // visible tasks are specified by Gradle as those that have a non-empty group
-            if (projectTask.isPublic()) {
-                publicTasks.add(task.getName());
-            }
+            taskSelectors.put(Path.from(project.getPath()), taskSelector);
         }
 
         for (GradleProject childProject : project.getChildren()) {
-            collectAllTasksRecursively(childProject, tasksWithDescription, publicTasks, enforceAllTasksPublic);
+            taskSelectors.putAll(getAllTaskSelectorsByProjectPath(childProject, enforceAllTasksPublic));
         }
+
+        return taskSelectors.build();
+    }
+
+    private static TreeBasedTable<String, Path, OmniProjectTask> getAllProjectTasksByNameAndPath(GradleProject project, boolean enforceAllTasksPublic) {
+        TreeBasedTable<String, Path, OmniProjectTask> tasks = TreeBasedTable.create(Ordering.natural(), Path.Comparator.INSTANCE);
+        for (GradleTask task : project.getTasks()) {
+            OmniProjectTask projectTask = DefaultOmniProjectTask.from(task, enforceAllTasksPublic);
+            tasks.put(projectTask.getName(), projectTask.getPath(), projectTask);
+        }
+
+        for (GradleProject childProject : project.getChildren()) {
+            tasks.putAll(getAllProjectTasksByNameAndPath(childProject, enforceAllTasksPublic));
+        }
+        return tasks;
     }
 
     /**
