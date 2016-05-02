511598af8adb5de54c0ea317b6016fd63aff77b5
Stefan Oehme <st.oehme@gmail.com>
2016-05-02 13:00:10 +0200

    Fixed another task visibility test

--- toolingmodel/src/test/groovy/com/gradleware/tooling/toolingmodel/repository/internal/DefaultModelRepositoryTest.groovy
+++ toolingmodel/src/test/groovy/com/gradleware/tooling/toolingmodel/repository/internal/DefaultModelRepositoryTest.groovy
@@ -745,13 +745,13 @@ class DefaultModelRepositoryTest extends ToolingModelToolingClientSpecification 
     myFirstTaskOfSub1.name == 'myFirstTaskOfSub1'
     myFirstTaskOfSub1.description == '1st task of sub1'
     myFirstTaskOfSub1.path.path == ':sub1:myFirstTaskOfSub1'
-    myFirstTaskOfSub1.isPublic() == (!higherOrEqual('2.1', distribution) || higherOrEqual('2.3', distribution))
+    myFirstTaskOfSub1.isPublic() == (!higherOrEqual('2.1', distribution) || !higherOrEqual("2.3", distribution) && environment != Environment.ECLIPSE || higherOrEqual('2.3', distribution))
 
     def mySecondTaskOfSub1 = sub1ExplicitTasks[1]
     mySecondTaskOfSub1.name == 'mySecondTaskOfSub1'
     mySecondTaskOfSub1.description == '2nd task of sub1'
     mySecondTaskOfSub1.path.path == ':sub1:mySecondTaskOfSub1'
-    mySecondTaskOfSub1.isPublic() == !higherOrEqual('2.3', distribution)
+    mySecondTaskOfSub1.isPublic() == !higherOrEqual('2.1', distribution)
 
     def projectSub2 = buildInvocations.get(Path.from(':sub2')).get()
     def sub2ExplicitTaskSelectors = projectSub2.taskSelectors.findAll { !ImplicitTasks.ALL.contains(it.name) }
@@ -761,7 +761,7 @@ class DefaultModelRepositoryTest extends ToolingModelToolingClientSpecification 
     myTaskSelector.name == 'myTask'
     myTaskSelector.description != null
     myTaskSelector.projectPath.path == ':sub2'
-    myTaskSelector.isPublic() == (!higherOrEqual('2.1', distribution) || higherOrEqual('2.3', distribution))
+    myTaskSelector.isPublic() == (!higherOrEqual('2.1', distribution) || !higherOrEqual("2.3", distribution) && environment != Environment.ECLIPSE || higherOrEqual('2.3', distribution))
     myTaskSelector.selectedTaskPaths*.path as List == (!higherOrEqual('1.12', distribution) || !higherOrEqual('2.3', distribution) && environment == Environment.ECLIPSE ? [':sub2:myTask', ':sub2:subSub1:myTask'] : [])
     // empty selected task paths for task selectors from 'authentic' build invocations
 
